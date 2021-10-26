32cd9fb164f7336eeb45232a6fd8a7fc5ab62ac5
Enã Lins <ena.mlins@gmail.com>
2021-10-26 19:54:06 -0300

    Reduced html, better css and mobile

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
+//ICONS
 let circle = '<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-circle fa-w-16" style="height: 100% ;margin-bottom: -1px;"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z" class=""></path></svg>';
 
 let times = '<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-times fa-w-10 fa-2x" style="height: 100% ;margin-bottom: -5px;"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z" class=""></path></svg>';
 
-// Tebuleiro do jogo
-var tabuleiro = [];
-// Jogadas possíveis
-var possiveis = ['1x1', '1x2', '1x3', '2x1', '2x2', '2x3', '3x1', '3x2', '3x3'];
-// Conjuntos de jogadas que define uma vitória
-let vitorias = [{a: '1x1', b: '1x2', c: '1x3'}, {a: '2x1', b: '2x2', c: '2x3'}, {a: '3x1', b: '3x2', c: '3x3'}, {a: '1x1', b: '2x1', c: '3x1'}, {a: '1x2', b: '2x2', c: '3x2'}, {a: '1x3', b: '2x3', c: '3x3'}, {a: '1x1', b: '2x2', c: '3x3'}, {a: '1x3', b: '2x2', c: '3x1'}];
+let play = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-play fa-w-14 fa-2x"><path fill="#ffffff" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" class=""></path></svg>';
 
-let placHuman = 0;
-let placRobot = 0;
+let reset = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sync-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-sync-alt fa-w-16"><path fill="#ffffff" d="M370.72 133.28C339.458 104.008 298.888 87.962 255.848 88c-77.458.068-144.328 53.178-162.791 126.85-1.344 5.363-6.122 9.15-11.651 9.15H24.103c-7.498 0-13.194-6.807-11.807-14.176C33.933 94.924 134.813 8 256 8c66.448 0 126.791 26.136 171.315 68.685L463.03 40.97C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.749zM32 296h134.059c21.382 0 32.09 25.851 16.971 40.971l-41.75 41.75c31.262 29.273 71.835 45.319 114.876 45.28 77.418-.07 144.315-53.144 162.787-126.849 1.344-5.363 6.122-9.15 11.651-9.15h57.304c7.498 0 13.194 6.807 11.807 14.176C478.067 417.076 377.187 504 256 504c-66.448 0-126.791-26.136-171.315-68.685L48.97 471.03C33.851 486.149 8 475.441 8 454.059V320c0-13.255 10.745-24 24-24z" class=""></path></svg>';
+
+let humanIcon = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-user fa-w-14" style="height: 35px;"><path fill="#FFFFFF" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z" class=""></path></svg>';
+
+let robotIcon = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-robot" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-user-robot fa-w-14" style="height: 35px;"><path fill="#FFFFFF" d="M17.99986,256H48V128H17.99986A17.9784,17.9784,0,0,0,0,146v92A17.97965,17.97965,0,0,0,17.99986,256Zm412-128H400V256h29.99985A17.97847,17.97847,0,0,0,448,238V146A17.97722,17.97722,0,0,0,429.99985,128ZM116,320H332a36.0356,36.0356,0,0,0,36-36V109a44.98411,44.98411,0,0,0-45-45H241.99985V18a18,18,0,1,0-36,0V64H125a44.98536,44.98536,0,0,0-45,45V284A36.03685,36.03685,0,0,0,116,320Zm188-48H272V240h32ZM288,128a32,32,0,1,1-32,32A31.99658,31.99658,0,0,1,288,128ZM208,240h32v32H208Zm-32,32H144V240h32ZM160,128a32,32,0,1,1-32,32A31.99658,31.99658,0,0,1,160,128ZM352,352H96A95.99975,95.99975,0,0,0,0,448v32a32.00033,32.00033,0,0,0,32,32h96V448a31.99908,31.99908,0,0,1,32-32H288a31.99908,31.99908,0,0,1,32,32v64h96a32.00033,32.00033,0,0,0,32-32V448A95.99975,95.99975,0,0,0,352,352ZM176,448a15.99954,15.99954,0,0,0-16,16v48h32V464A15.99954,15.99954,0,0,0,176,448Zm96,0a16,16,0,1,0,16,16A15.99954,15.99954,0,0,0,272,448Z" class=""></path></svg>';
+
+//botões
+let newGame = document.getElementById('newGame'),
+resetScore = document.getElementById('resetScore'),
+
+// Tebuleiro do jogo
+tabuleiro = [],
+// Jogadas possíveis
+possiveis = ['1x1', '1x2', '1x3', '2x1', '2x2', '2x3', '3x1', '3x2', '3x3'],
+// Conjuntos de jogadas que define uma vitória
+vitorias = [{a: '1x1', b: '1x2', c: '1x3'}, {a: '2x1', b: '2x2', c: '2x3'}, {a: '3x1', b: '3x2', c: '3x3'}, {a: '1x1', b: '2x1', c: '3x1'}, {a: '1x2', b: '2x2', c: '3x2'}, {a: '1x3', b: '2x3', c: '3x3'}, {a: '1x1', b: '2x2', c: '3x3'}, {a: '1x3', b: '2x2', c: '3x1'}],
+
+//placares
+placHuman = 0,
+placRobot = 0;
+
+newGame.children[0].innerHTML = play;
+resetScore.children[0].innerHTML = reset;
+document.getElementById('humanIcon').innerHTML = humanIcon;
+document.getElementById('robotIcon').innerHTML = robotIcon;
 
 function addOne(x){
   if(x == 'human'){
